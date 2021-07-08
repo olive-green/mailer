b2bdaf0571c591948c673886ef036c2712466fec
olive-green <pk18538984@gmail.com>
2021-07-08 19:28:33 +0530

    cors added

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express=require("express");
 const app=express();
 const nodemailer=require("nodemailer");
 const bodyParser=require("body-parser");
+const cors=require('cors')
 const {google} =require("googleapis");
 require("dotenv").config();
 
 app.set("view engine","ejs");
 app.use(express.static("public"));
+app.use(cors());
 app.use(express.json());
 
 app.use(bodyParser.urlencoded({extended:true}));
